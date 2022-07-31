b8a6a1255be6b636395371d78aade1504003a445
jaoluiz13 <luizcostajoao12@gmail.com>
2022-07-31 16:37:39 -0300

    feat : new features

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,47 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
+import { ICategoryRepository } from "../../repositories/ICategoryRepository";
+
+interface IImportyCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  loadCategory(file: Express.Multer.File): Promise<IImportyCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IImportyCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategory(file);
+    categories.map((category) => {
+      const { name, description } = category;
+      const existsCategory = this.categoryRepository.findByName(name);
+      if (!existsCategory) {
+        this.categoryRepository.create({ name, description });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };

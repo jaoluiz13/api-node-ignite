3b603852962231647b9c5caa8e5176e3200c2ccf
jaoluiz13 <luizcostajoao12@gmail.com>
2022-08-06 11:04:23 -0300

    feat: new features

---
+++ src/utils/file.ts
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+import { AppError } from "../errors/AppErrors";
+
+export const deleteFile = (fileName: string) => {
+  try {
+    fs.stat(fileName, (err, stat) => {
+      return stat;
+    });
+  } catch (err) {
+    return;
+  }
+  fs.unlink(fileName, (err) => {
+    throw new AppError("File not found");
+  });
+};

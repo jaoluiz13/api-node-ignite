094275a5472cc0c54fd3a7cd7d2e8fb2af5b2fd2
jaoluiz13 <luizcostajoao12@gmail.com>
2022-08-27 15:01:17 -0300

    feat: new features

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { ICarImageRepository } from "../../repositories/ICarImageRepository";
 
 interface IRequest {
@@ -11,11 +12,14 @@ interface IRequest {
 class UploadCarImageUseCase {
   constructor(
     @inject("CarImageRepository")
-    private carImageRepository: ICarImageRepository
+    private carImageRepository: ICarImageRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
--- src/shared/infra/http/router/user.routes.ts
+++ src/shared/infra/http/router/user.routes.ts
@@ -3,16 +3,18 @@ import multer from "multer";
 
 import uploadConfig from "../../../../config/upload";
 import { CreateUserController } from "../../../../modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "../../../../modules/accounts/useCases/profileUser/ProfileUserController";
 import { UpdateUserAvatarController } from "../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const userRoutes = Router();
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
-
+const profileUserController = new ProfileUserController();
 const uploadAvatar = multer(uploadConfig);
 
 userRoutes.post("/create", createUserController.handle);
+userRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 userRoutes.patch(
   "/avatar",
   ensureAuthenticated,

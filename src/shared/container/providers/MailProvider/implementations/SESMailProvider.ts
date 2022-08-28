0b1a0b7491cb504c7445e9c3f1def7eeba0babb2
jaoluiz13 <luizcostajoao12@gmail.com>
2022-08-28 16:25:33 -0300

    feat: new features

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,39 @@
+import { SES } from "aws-sdk";
+import fs from "fs";
+import handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+  constructor() {
+    this.client = nodemailer.createTransport({
+      SES: new SES({
+        apiVersion: "2010-12-01",
+        region: process.env.AWS_SES_REGION,
+      }),
+    });
+  }
+  async sendEmail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+    const templateParser = handlebars.compile(templateFileContent);
+    const templateHtml = templateParser(variables);
+
+    await this.client.sendMail({
+      to,
+      from: "Rentx<joaoluiz@joaoluizcst.com.br>",
+      subject,
+      html: templateHtml,
+    });
+  }
+}
+
+export { SESMailProvider };
